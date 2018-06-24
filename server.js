2d1d1ae85a45277e18dcf61b6821a537951a83f0
yangkaihong <807632219@qq.com>
2018-06-24 14:39:26 +1000

    https enabled

--- server.js
+++ server.js
@@ -2,8 +2,14 @@ var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
 var router = require('./app/routes/server.routes')
-
 var app = express()
+var https = require('https');
+var fs = require('fs');
+
+var options = {
+	key: fs.readFileSync('./private.key'),
+	cert: fs.readFileSync('./certificate.crt')
+}
 
 //console.log(__dirname);
 app.set('views', path.join(__dirname,'/app/views'));
@@ -12,8 +18,11 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded())
 app.use('/',router)
 
-app.listen(3000, function () {
+/*app.listen(3000, function () {
 	  console.log('Plot app listening on port 3000!')
-	})
+	})*/
+https.createServer(options, app).listen(3000, function () {
+    console.log('Https server listening on port ' + 3000);
+});
 	
 module.exports = app;
